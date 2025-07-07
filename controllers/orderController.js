f7a1bb4e2f6660363b09b24b6044af2ab6cbc46e
unknown <patilsarika456654@gmail.com>
2025-07-07 13:01:47 +0530

    Payment and email

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -6,8 +6,8 @@ const Cart = require("../models/Cart");
 require("dotenv").config();
 
 const razorpay = new Razorpay({
-  key_id: process.env.Razorpay_KEY_ID,
-  key_secret: process.env.Razorpay_KEY_SECRET,
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
 const createRazorpayOrder = async (req, res) => {
